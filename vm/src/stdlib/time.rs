465597710b9b792e4e9a10f4d60603af5c6714db
Jordan Last <jordan.michael.last@gmail.com>
2022-12-01 14:31:33 -0600

    attempt to add ic_cdk::time

--- vm/src/stdlib/time.rs
+++ vm/src/stdlib/time.rs
@@ -104,7 +104,7 @@ mod time {
 
     #[cfg(all(target_arch = "wasm32", not(target_os = "wasi")))]
     fn _time(_vm: &VirtualMachine) -> PyResult<f64> {
-        Ok(0.0) // TODO this would need to be implemented with ic_cdk::time()
+        Ok(ic_cdk::time() / 1_000_000_000)
     }
 
     #[pyfunction]
